4f30e3b9770da2fb33e4d143ff7cde832b298bf9
Ivan Sidarau <ivan.sidarau@disfa.com>
2012-03-06 10:45:09 -0500

    creating lottos redmine plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_lottos do
+  name 'Redmine Lottos plugin'
+  author 'Ivan Sidarau'
+  description 'This is a plugin for Redmine, for creating one day lottos. '
+  version '0.0.1'
+  url 'http://no-url.yet'
+  author_url 'http://sidorovis.blogspot.com'
+end
